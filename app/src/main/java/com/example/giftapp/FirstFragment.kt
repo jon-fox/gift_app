9ccc7249d62b0b75a8c8b8be9f72a6e98ea734d7
foxj7 <foxj77.jf@gmail.com>
2023-06-11 00:12:35 -0500

    design changes

--- app/src/main/java/com/example/giftapp/FirstFragment.kt
+++ app/src/main/java/com/example/giftapp/FirstFragment.kt
@@ -2,4 +2,5 @@ import androidx.fragment.app.Fragment
 import com.example.giftapp.R
 
 class FirstFragment:Fragment(R.layout.fragment_first) {
+
 }
--- app/src/main/java/com/example/giftapp/MainActivity.kt
+++ app/src/main/java/com/example/giftapp/MainActivity.kt
@@ -4,15 +4,16 @@ import FirstFragment
 import SecondFragment
 import ThirdFragment
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CalendarView
+import android.widget.*
 import android.widget.CalendarView.OnDateChangeListener
-import android.widget.EditText
-import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     // on below line we are creating
@@ -20,13 +21,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var dateTV: TextView
     lateinit var calendarView: CalendarView
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // initializing variables of
         // list view with their ids.
-//        dateTV = findViewById(R.id.idTVDate)
+        dateTV = findViewById(R.id.idTVDate)
 
         val firstFragment=FirstFragment()
         val secondFragment=SecondFragment()
@@ -52,6 +54,14 @@ class MainActivity : AppCompatActivity() {
                     dateTV.setText(Date)
                 })
 
+        val plusImageView: Button = findViewById(R.id.plusImageView)
+        plusImageView.setOnClickListener {
+            val currentDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+            dateTV.text = currentDate
+        }
+
+//        val plusImageView: Button = findViewById(R.id.plusImageView)
+// Apply additional customizations to the plusImageView if required
 //        val bottomNavigationView = (R.id.bottomNavigationView)
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
@@ -61,7 +71,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.home->setCurrentFragment(firstFragment)
                 R.id.person->setCurrentFragment(secondFragment)
                 R.id.settings->setCurrentFragment(thirdFragment)
-
             }
             true
         }
